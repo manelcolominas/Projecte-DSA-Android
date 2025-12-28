98dca3e16678da6bcc2b264e13b83541e269cbf5
RogerMacia <roger.macia@estudiantat.upc.edu>
2025-12-28 17:40:44 +0100

    Versió AndroidX i altres per compatibilitat amb Unity

--- app/src/main/java/dsa/upc/edu/listapp/ShopActivity.java
+++ app/src/main/java/dsa/upc/edu/listapp/ShopActivity.java
@@ -3,7 +3,7 @@ package dsa.upc.edu.listapp;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
--- app/src/main/java/dsa/upc/edu/listapp/SplashScreenActivity.java
+++ app/src/main/java/dsa/upc/edu/listapp/SplashScreenActivity.java
@@ -3,7 +3,7 @@ package dsa.upc.edu.listapp;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
